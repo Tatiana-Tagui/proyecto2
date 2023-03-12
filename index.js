ed3814fd4ac1db03499d91e54bc6b6c0d0127eb1
proyecto1 <tati1bustos1@gmail.com>
2023-03-12 18:47:43 -0300

    se crea nuevo proyecto

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "/index.css";
+import App from "./components/App";
+import { CarritoProvider } from "./context/CartContext";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <CarritoProvider>
+    <App />
+  </CarritoProvider>
+);
